34d4ff1bea631a630c32978fc1e802e4e9ab302e
eunjeong <eunjeong0911@naver.com>
2025-11-27 10:30:29 +0900

    chore: 추가

--- apps/frontend/src/pages/WishListPage.tsx
+++ apps/frontend/src/pages/WishListPage.tsx
@@ -6,6 +6,7 @@
  * - Header.tsx (헤더)
  * - LandImage.tsx (매물 이미지)    
  * - LandList.tsx (매물 목록)
+ * - Pagination.tsx (페이지네이션)
  * - Button.tsx (비교하기 버튼)
  * - Comparison.tsx (매물 비교)
  * - Footer.tsx (푸터)
