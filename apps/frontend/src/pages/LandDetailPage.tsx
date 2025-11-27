bce1a200acb1959d0de5f949b9a9bf9c4433246c
eunjeong <eunjeong0911@naver.com>
2025-11-27 10:24:05 +0900

    chore: Pages폴더 파일 추가

---
+++ apps/frontend/src/pages/LandDetailPage.tsx
@@ -0,0 +1,16 @@
+/**
+ * LandDetailPage
+ * 
+ * 매물 상세 페이지
+ * 
+ * - Header.tsx (헤더)
+ * - Chatbot.tsx (챗봇)
+ * - LandDetail.tsx (매물 상세)
+ * - LandImage.tsx (매물 이미지)
+ * - Map.tsx (지도)
+ * - MarkerInfo.tsx (마커 정보)
+ * - CommunityCard.tsx (커뮤니티 게시글 카드)
+ * - LandList.tsx (매물 목록)
+ * - Footer.tsx (푸터)
+ * 
+ */
---
+++ apps/frontend/src/pages/MainPage.tsx
@@ -0,0 +1,15 @@
+/**
+ * MainPage
+ * 
+ * 메인 페이지
+ * 
+ * - Header.tsx (헤더)
+ * - Chatbot.tsx (챗봇)
+ * - PreferenceFilter.tsx (선호도 필터)
+ * - Map.tsx (지도)
+ * - LandListFilter.tsx (매물 목록)
+ * - LandList.tsx (매물 목록)
+ * - Temperature.tsx (온도)
+ * - TemperatureList.tsx (온도 목록)
+ * - Footer.tsx (푸터)
+ */
