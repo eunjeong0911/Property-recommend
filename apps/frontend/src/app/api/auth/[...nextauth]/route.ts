7c2b13c30b391d9feb01177ab0b130a502abb837
william7333 <jinth143@gmail.com>
2025-11-27 16:23:39 +0900

    🎉 Login컴포넌트,login페이지
    구글 로그인 연동하여 컴포넌트 및 페이지 개발 완료. 로그인페이지에 AuthProvider를 추가하여 next.js 와 구글로그인 API를 연동하였음.

---
+++ apps/frontend/src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,24 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID ? "Loaded" : "Missing");
+console.log("GOOGLE_CLIENT_SECRET:", process.env.GOOGLE_CLIENT_SECRET ? "Loaded" : "Missing");
+
+const handler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+        }),
+    ],
+    callbacks: {
+        async session({ session, token }) {
+            return session;
+        },
+        async jwt({ token, user }) {
+            return token;
+        },
+    },
+});
+
+export { handler as GET, handler as POST };
