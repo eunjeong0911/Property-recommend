fe513ba7a9875caa19c450925e477f788a0f3422
william7333 <jinth143@gmail.com>
2025-12-24 18:21:38 +0900

    ⚡️ Docker 데이터베이스 연동 및 임베딩파일 수정

--- apps/backend/config/settings/base.py
+++ apps/backend/config/settings/base.py
@@ -175,6 +175,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
 }
 
 # JWT 설정
